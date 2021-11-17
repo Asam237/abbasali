6f9276419dc1aff3eb0159256151e103384a5687
Asam237 <abbasalimokolo@gmail.com>
2021-11-17 11:43:45 +0100

    Change homepage in package file

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,32 +1,32 @@
 import React, { useEffect } from "react";
 import { About } from "../../components/about/About.component";
 import { Footer } from "../../components/footer/Footer.component";
-import { Header } from "../../components/header/Header.component"
+import { Header } from "../../components/header/Header.component";
 import { HireMe } from "../../components/hire-me/HireMe.component";
 import { Intro } from "../../components/intro/Intro.component";
 import { Service } from "../../components/service/Service.component";
-import AOS from "aos"
-import "aos/dist/aos.css"
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export const Home = () => {
+  useEffect(() => {
+    AOS.init({
+      duration: 300,
+      disable: "mobile",
+    });
+    document.title = "Abba SAli";
+  }, []);
 
-    useEffect(() => {
-        AOS.init({
-            duration: 300,
-            disable: "mobile"
-        })
-    }, [])
-
-    return (
-        <div id="home" className="site">
-            <Header />
-            <main>
-                <Intro />
-                <About />
-                <Service />
-                <HireMe />
-            </main>
-            <Footer />
-        </div>
-    )
-}
+  return (
+    <div id="home" className="site">
+      <Header />
+      <main>
+        <Intro />
+        <About />
+        <Service />
+        <HireMe />
+      </main>
+      <Footer />
+    </div>
+  );
+};
